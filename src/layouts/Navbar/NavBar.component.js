70d3f1875614fbc2665e12a8371f786659ad0797
Mina Amir <mina.amir@scandiweb.com>
2023-03-08 13:39:59 +0200

    Adding tailwind css with configs

--- src/layouts/Navbar/NavBar.component.js
+++ src/layouts/Navbar/NavBar.component.js
@@ -5,13 +5,13 @@ export default function NavBar({nav_items}) {
     //console.log("NAVBAR COMP",page_items);
   return (
     <div>
-        <ul>
+        <ul className='list-disc list-inside'>
             {nav_items.map((nav_item, index) =>
                 <li key={index}>
                     <Link to={'/cat'+nav_item.main_category_route}>
                       {nav_item.main_category_title}</Link>
                       {nav_item.has_mega && <ul>
-                      {nav_item.mega_group.mega_repeater.map((group_item,index) =><li key={index}><Link to={'/cat'+group_item.mega_item_route}>{group_item.mega_item_title}</Link></li>)}
+                      {nav_item.mega_group.mega_repeater.map((group_item,index) =><li  key={index}><Link to={'/cat'+group_item.mega_item_route}>{group_item.mega_item_title}</Link></li>)}
                       </ul>}
                 </li>
                 )
